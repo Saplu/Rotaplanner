6ecaa7dede20ab8428eb2e39a2372310f1d90208
Halti <huhtala.h@hotmail.fi>
2020-07-18 15:08:32 +0300

    Removed gitignore, shit already hit the fan in terms of security

---
+++ RotaplannerConsole/DataAccess/ConnectionString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public static class ConnectionString
+    {
+        public static readonly string conn = "mongodb+srv://Saplu:87se77y6k7rkioh7mvfz@dcdata.lt2xw.mongodb.net/dcdata?retryWrites=true&w=majority";
+        public static string connection { get; set; }
+    }
+}
